3b05958af4277cb602abb9266079df19a607ee89
Ruben-Dean <dean.caldeira@gmail.com>
2018-04-12 15:07:13 +0100

    added exercise

--- Exercisesintermediate/main.js
+++ Exercisesintermediate/main.js
@@ -14,20 +14,62 @@ function fizzbuzz() {
 }
 
 function ite4(){
+    var num = document.getElementById("numberinput").value.replace();
+    var count = 0;
 
-        var count = 0;
-        console.log("Input: " + input)
-        while(input > 1){
+        while(num != 1) {
+            if (num % 3==0){
+                num /=3;
+
+                count++
+                console.log(num)
+            }
+            if((num - 1) % 3==0){
+                num= +num-1;
+                count++;
+                console.log(num);
+            } else if ((num +1) % 3==0){
+                num= +num-1;
+                count++;
+                console.log(num);
+            }
+
+        }
+
+        console.log("number of iterations: " + count);
+
+      /*  console.log("Input: " + input);
+        for(var i = 0; input > 0; i++){
             if(input % 3 == 0){
-                console.log("divided by three")
+                console.log("divided by three");
                 input /= 3;
             } else {
-                console.log("subtracted by 1")
-                input--;
-                //input /= 3;
+                console.log("added by 1");
+                input++;
+
             }
-            count++;
             console.log("current input: " + input)
         }
-    console.log("Final Count: " + count);
+    console.log("Final Count: " + i);*/
+
+        /*  var input = document.getElementById('input');
+          var times = parseInt(input.value);
+
+          for (var i = 0; i < times; i++){
+
+              console.log(input);*/
+
+             /* if(input % 3){
+                  console.log("divided by three");
+                  input / 3;
+              } else {
+                  console.log("added by 1")
+                  input++
+              }
+              console.log("current input: " + input)
+
+          }
+            console.log("Final Count: " + input)*/
+
+
 }
